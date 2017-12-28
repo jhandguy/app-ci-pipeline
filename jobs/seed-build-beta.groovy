dd49e30b97dabccb925325cbc11e85bc392af39a
jean.mainguy <jean.mainguy@egym.de>
2017-12-28 19:37:24 +0100

    Fix broken Pipeline

--- jobs/seed-build-beta.groovy
+++ jobs/seed-build-beta.groovy
@@ -1,8 +1,23 @@
 pipelineJob('build-beta') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('build-beta.groovy'))
-            sandbox()
+  def repo = 'https://github.com/jhandguy/android-ci-pipeline'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('pipelines/build-beta.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
         }
+
+        // the single line below also works, but it
+        // only covers the 'master' branch and may not give you
+        // enough control.
+        // git(repo, 'master', { node -> node / 'extensions' << '' } )
+      }
     }
+  }
 }
--- jobs/seed-build-qa.groovy
+++ jobs/seed-build-qa.groovy
@@ -1,8 +1,23 @@
 pipelineJob('build-qa') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('build-qa.groovy'))
-            sandbox()
+  def repo = 'https://github.com/jhandguy/android-ci-pipeline'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('pipelines/build-qa.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
         }
+
+        // the single line below also works, but it
+        // only covers the 'master' branch and may not give you
+        // enough control.
+        // git(repo, 'master', { node -> node / 'extensions' << '' } )
+      }
     }
+  }
 }
