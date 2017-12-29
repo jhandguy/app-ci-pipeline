3a84e3c0fc194ee222bb0f02ecf7f307ad25d627
jean.mainguy <jean.mainguy@egym.de>
2017-12-29 13:40:46 +0100

    Fix broken Pipeline

--- jobs/build_qa.groovy
+++ jobs/build_qa.groovy
@@ -6,15 +6,10 @@ pipelineJob('build-qa') {
       scm {
         git {
           remote { url("https://github.com/jhandguy/${platform}-ci-pipeline") }
-          branches('master')
+          branches('master', '**/feature*')
           scriptPath('build-qa/Jenkinsfile')
-          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+          extensions { }
         }
-
-        // the single line below also works, but it
-        // only covers the 'master' branch and may not give you
-        // enough control.
-        // git(repo, 'master', { node -> node / 'extensions' << '' } )
       }
     }
   }
